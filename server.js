1212c6915fc32cb5f79a76e2304bf6b6e9490342
amol25sep1989 <amol25sep1989@gmail.com>
2019-02-13 12:05:02 +0530

    Changes

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 })
 
+/**Added for heroku deployment */
+
+
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
